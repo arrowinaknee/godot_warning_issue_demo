74190fb7e5ce0a739a01f62486fbbd9e374e4f06
ArrowInAKnee <gatexxl@gmail.com>
2020-03-22 11:18:50 +0300

    Initial commit

---
+++ AnimatedSprite.gd
@@ -0,0 +1,5 @@
+extends AnimatedSprite
+tool
+
+func _get_configuration_warning():
+	return "This message won't be displayed"
---
+++ Bone2D.gd
@@ -0,0 +1,5 @@
+extends Bone2D
+tool
+
+func _get_configuration_warning():
+	return "But this message will"
